45f126bfdd0d3746dea4cd036b6dfdcfdafdbcd0
Jam <superwangjianmin@163.com>
2019-12-04 17:26:07 +0800

    Initial commit

---
+++ Example/YDCTMediatorSDK/YDViewController.h
@@ -0,0 +1,13 @@
+//
+//  YDViewController.h
+//  YDCTMediatorSDK
+//
+//  Created by JamWangJam on 12/04/2019.
+//  Copyright (c) 2019 JamWangJam. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YDViewController : UIViewController
+
+@end
